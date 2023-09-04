539e1c17e42489f0a023d3bf57290a2086d73b1a
narendra <51484021+alenway@users.noreply.github.com>
2023-09-04 18:06:21 +0530

    header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
